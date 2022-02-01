5b734f2e9661a9714a7484239719f5d03f36e92a
nakanodesukun <94818124+nakanodesukun@users.noreply.github.com>
2022-02-01 21:12:46 +0900

    更新しました

--- TimerApp/ViewController.swift
+++ TimerApp/ViewController.swift
@@ -9,9 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer: Timer?
+    var repeatingTimer: Timer?
+
+    var count = 0
+
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateCountLabel()
+    }
+    func updateCountLabel() {
+        label.text = String(count)
+    }
+
+    func stopTimer() {
+        timer?.invalidate()
+        repeatingTimer?.invalidate()
+
+    }
+    @IBAction func startButton(_ sender: Any) {
+
+
+    }
+
+    @IBAction func startRepeatButton(_ sender: Any) {
+        stopTimer()
+        updateCountLabel()
+        repeatingTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [weak self]_ in
+            self?.count += 1
+            self?.updateCountLabel()
+
+        })
+    }
+    @IBAction func stopButton(_ sender: Any) {
+        stopTimer()
     }
 
 
